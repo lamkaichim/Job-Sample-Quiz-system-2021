55bb72803a791ec0a1b2d3ea9ff95a960572c778
stephenlkc <54799693+stephenlkc@users.noreply.github.com>
2021-06-22 18:03:33 +0800

    Delete sample/layout directory

--- sample/layout/scripts/transition.js
+++
@@ -1,9 +0,0 @@
-$( ".cta" ).click(function() {
-    $( ".transition").toggleClass( "anim-trans" );
-    setTimeout(returnInit, 4000);
-
-  });
-
-  function returnInit() {
-    location.href = "test.html";
-  }
